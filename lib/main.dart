5d06f9537592e457832e19d7762076f13b7026cc
Paulo Henrique Nonaka <paulo.nonaka@ifood.com.br>
2021-07-17 14:52:55 -0300

    Initial Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth_firestore/design_system/tokens/color_palette.dart';
 import 'package:flutter_firebase_auth_firestore/navigation/app_navigator.dart';
@@ -5,8 +6,10 @@ import 'package:get_it/get_it.dart';
 import 'design_system/tokens/text_theme.dart';
 import 'di/container.dart' as di;
 
-void main() {
+Future<void> main() async {
   di.setup();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
